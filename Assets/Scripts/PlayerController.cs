0c14ea0f1f0478d0824395d2db1c087f54e1cd32
Bloodskys <89423516dD>
2017-04-23 08:51:15 +0300

    Init commit. Contains: --Map with platforms --Hero with animation (not completed)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 2f;
+    public bool facingRight = true;
+    public float jumpForce;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private bool lastWalkState;
+    private bool lastJumpState;
+    private bool lastFallState;
+    private bool lastCrouchState;
+    enum State
+    {
+        Idle,
+        Walk,
+        Jump,
+        Fall,
+        Crouch
+    }
+    State state;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        SetState(State.Idle);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void FixedUpdate()
+    {
+        float move = /*1f;//*/ Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+		//Crouch 
+        if (Input.GetKey(KeyCode.S))
+        {
+            SetState(State.Crouch);
+        }
+        else
+        {
+            #region Walk
+            if (Mathf.Abs(move) > 0 && lastWalkState == false)
+            {
+                SetState(State.Walk);
+            }
+            else if (Mathf.Abs(move) == 0 && lastWalkState == true)
+            {
+                SetState(State.Idle);
+            }
+            #endregion
+            #region Jump
+            if (rb.velocity.y > 0 && lastJumpState == false)
+            {
+                SetState(State.Jump);
+            }
+            else if (rb.velocity.y < 0 && lastFallState == false)
+            {
+                SetState(State.Fall);
+            }
+            else if (rb.velocity.y == 0 && (lastFallState || lastJumpState))
+            {
+                SetState(State.Idle);
+            }
+            #endregion
+        }
+    }
+
+    void SetState(State state)
+    {
+        switch (state)
+        {
+            case State.Crouch:
+
+                break;
+            case State.Fall:
+                anim.SetBool("Walk", false);
+                anim.SetBool("Jump", false);
+                anim.SetBool("Fall", true);
+                anim.SetBool("Crouch", false);
+                break;
+            case State.Idle:
+                anim.SetBool("Walk", false);
+                anim.SetBool("Jump", false);
+                anim.SetBool("Fall", false);
+                anim.SetBool("Crouch", false);
+                break;
+            case State.Jump:
+                anim.SetBool("Walk", false);
+                anim.SetBool("Jump", true);
+                anim.SetBool("Fall", false);
+                anim.SetBool("Crouch", false);
+                break;
+            case State.Walk:
+                anim.SetBool("Walk", true);
+                anim.SetBool("Jump", false);
+                anim.SetBool("Fall", false);
+                anim.SetBool("Crouch", false);
+                break;
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
